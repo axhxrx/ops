169a04fa53fda02f7ed7e18e9acbe942e7ebacc9
Mason <mason@axhxrx.com>
2025-10-16 16:05:45 +0900

    🧹 Add monorepo/megarepo config

---
+++ mod.ts
@@ -0,0 +1,10 @@
+// Main exports for @axhxrx/ops
+export { ConfirmOp } from './ConfirmOp';
+export { ResultOp, SelectNameOp, WelcomeOp } from './GameOps';
+export { InputTextOp } from './InputTextOp';
+export type { IOContext } from './IOContext';
+export { Op } from './Op';
+export { OpRunner } from './OpRunner';
+export type { Outcome } from './Outcome';
+export { PrintOp } from './PrintOp';
+export { SelectFromListOp } from './SelectFromListOp';
